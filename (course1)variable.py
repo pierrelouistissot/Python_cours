d977ea473e6121eeef5c54581aed572143ea5dcd
pierrelouistissot <145775418+pierrelouistissot@users.noreply.github.com>
2023-10-23 11:40:02 +0200

    Add files via upload

---
+++ (course1)variable.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 10:36:11 2023
+
+@author: tisso
+"""
+
+print("Goodbye,cruel world!")
+
+type('Name') #permet de donner le type
+name = '431'
+name2 = '56'
+add = name + name2
+print(add)
+
+name = 431
+name2 = 21
+add= name + name2
+print(add)
+452
+print(""" hello python,... how areyou doing""")
+
+print("hello python\how are you doing")
+
+print("hello python\nhow are you doing")
+
+
+
+num1=5
+num2=25
+add=num1+num2
+print(add)
+
+div=num2/num1
+print(div)
+
+mult=num1*num2
+print(mult)
+
+exp=num1**num2
+print(exp)
+
+
+
+num1= 3,14
+type(num1)
+
+num2=3.14
+num3=1.7128
+print(num2+num3)
+num4=str(num2)
+num5=str(num3)
+print(num4+num5)
+
+
+
+print("coffee\n café\n caffé\n Kaffee")
+print('coffee\ncafé\ncaffé\nKaffee')
+
+
+
+print("How much?") 
+a=input(2.7)
+print("a"+2.5)
+
+
+
+pay= float(input("How much?"))
+print(pay+2.5)
+
+
+'sparrow'>'eagle'
+True
+'dog'>'cat' or 45%3==0
+True
+60-45/5+10==1
+False
+
+
+
+
+
